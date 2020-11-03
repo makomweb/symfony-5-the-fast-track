ee2f5301ddbf7026c0defd0a63f3abc5ef5584ba
Martin Komischke <martin.komischke@outlook.com>
2020-11-03 17:05:02 +0100

    alternative approach: passing an argument

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,20 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/hello/{name}", name="homepage")
      */
-    public function index(Request $request): Response
+    public function index(string $name): Response
     {
         $greet = '';
 
-        if ($name = $request->query->get('hello')) {
+        if ($name) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         } 
 
