38a2f88b854642f358d12224a03b81ab356f03b1
Martin Komischke <martin.komischke@outlook.com>
2020-11-08 12:45:39 +0100

    write functional test for the conference page

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,20 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        //$this->assertSelectorExists('div:contains("There are 1 comments.")');
+        $this->assertSelectorTextContains('div', 'There are 1 comments.');
+    }
 }
