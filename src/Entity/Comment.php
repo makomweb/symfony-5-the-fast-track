eaa76e23a2441ea07127ec11371061416eb9a474
Martin Komischke <martin.komischke@outlook.com>
2020-11-12 15:12:20 +0100

    adjust Comment entity with proper initialization of the state field

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -54,9 +54,9 @@ class Comment
     private $photoFilename;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, options={"default": "submitted"})
      */
-    private $state;
+    private $state = 'submitted';
 
     public function __toString(): string
     {
