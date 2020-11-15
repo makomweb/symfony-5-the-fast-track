4b2697f3a6dd2a3592ec425934e2333b5979d0e4
Martin Komischke <martin.komischke@outlook.com>
2020-11-15 15:07:05 +0100

    implement logic for cleaning up "old" comments

--- src/Command/CommentCleanupCommand.php
+++ src/Command/CommentCleanupCommand.php
@@ -2,8 +2,8 @@
 
 namespace App\Command;
 
+use App\Repository\CommentRepository;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,31 +11,38 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class CommentCleanupCommand extends Command
 {
+    private $commentRepository;
+
     protected static $defaultName = 'app:comment:cleanup';
+
+    public function __construct(CommentRepository $commentRepository)
+    {
+        $this->commentRepository = $commentRepository;
+
+        parent::__construct();
+    }
 
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Deletes rejected and spam comments from the database')
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Dry run')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        if ($input->getOption('dry-run')) {
+            $io->note('Dry mode enabled');
+
+            $count = $this->commentRepository->countOldRejected();
+        } else {
+            $count = $this->commentRepository->deleteOldRejected();
         }
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success(sprintf('Deleted "%d" old rejected/spam comments.', $count));
 
         return 0;
     }
