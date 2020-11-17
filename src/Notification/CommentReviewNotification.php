6a32b7c94ac43e161e445a0f0b5ab42eda501436
Martin Komischke <martin.komischke@outlook.com>
2020-11-17 16:42:46 +0100

    add the reject and accept link to the Slack chat message

--- src/Notification/CommentReviewNotification.php
+++ src/Notification/CommentReviewNotification.php
@@ -3,6 +3,7 @@
 namespace App\Notification;
 
 use App\Entity\Comment;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackActionsBlock;
 use Symfony\Component\Notifier\Bridge\Slack\Block\SlackDividerBlock;
 use Symfony\Component\Notifier\Bridge\Slack\Block\SlackSectionBlock;
 use Symfony\Component\Notifier\Bridge\Slack\SlackOptions;
@@ -16,10 +17,12 @@ use Symfony\Component\Notifier\Recipient\Recipient;
 class CommentReviewNotification extends Notification implements EmailNotificationInterface, ChatNotificationInterface
 {
     private $comment;
+    private $reviewUrl;
 
-    public function __construct(Comment $comment)
+    public function __construct(Comment $comment, string $reviewUrl)
     {
         $this->comment = $comment;
+        $this->reviewUrl = $reviewUrl;
 
         parent::__construct('New comment posted');
     }
@@ -49,6 +52,10 @@ class CommentReviewNotification extends Notification implements EmailNotificatio
                 ->block((new SlackSectionBlock())->text($this->getSubject()))
                 ->block(new SlackDividerBlock())
                 ->block((new SlackSectionBlock())
+                ->block((new SlackActionsBlock())
+                    ->button('Accept', $this->reviewUrl, 'primary')
+                    ->button('Reject', $this->reviewUrl.'?reject=1', 'danger')
+                )
                 ->text(sprintf('%s (%s) says: %s', $this->comment->getAuthor(), $this->comment->getEmail(), $this->comment->getText()))
             )
         );
