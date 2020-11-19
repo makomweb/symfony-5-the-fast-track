adf9229d5b44f8fe7e3075d330f2a43fec5e0f82
Martin Komischke <martin.komischke@outlook.com>
2020-11-19 15:14:04 +0100

    fix removing the cache folder in the start script

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-rm -r -fo .\var\cache\dev\http_cache\
+rmdir /q /s .\var\cache\dev\http_cache\
 docker-compose up -d
 symfony server:start -d
 symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async
